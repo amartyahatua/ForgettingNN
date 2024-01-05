79afd7a47638ef99c39360d032778927fa80ddbd
amartyahatua <amartyahatua@gmail.com>
2024-01-04 20:22:28 -0500

    Update README.md

--- train/code/intermediate_parameters.py
+++ train/code/intermediate_parameters.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import torch
+import numpy as np
 import torchvision
 import torchvision.transforms as transforms
 from model import Net
@@ -6,37 +8,50 @@ from model import Net
 transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
 batch_size = 4
-
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False, num_workers=2)
 activation = {}
+sorted_node = {}
+def sort_nodes(activation, name):
+    print('Name: ',name)
+    sort_index = np.argsort(activation[name] )
+    sorted_node[name] = torch.mean(sort_index.float(), axis=0)
+    l = pd.DataFrame(activation[name])
+    l = l.transpose()
+    m = pd.DataFrame(sorted_node[name])
+    n = pd.concat([l,m], axis=1)
+    n.to_csv(f'{name}_weights.csv')
+
 def get_activation(name):
     def hook(model, input, output):
         activation[name] = output.detach()
+        if(activation[name].dim() == 2):
+            print('2D array got in: ', name)
+            sort_nodes(activation, name)
     return hook
 
 def main():
     model = Net()
     model.load_state_dict(torch.load('../../model/cifar_net.pth'))
     model.eval()
-
     print(model)
-    activation = {}
 
     dataiter = iter(testloader)
     images, labels = next(dataiter)
 
-    model.conv1.register_forward_hook(lambda m, input, output: print(output.shape))
-    model.pool.register_forward_hook(lambda m, input, output: print(output.shape))
-    model.conv2.register_forward_hook(lambda m, input, output: print(output.shape))
-    model.fc1.register_forward_hook(lambda m, input, output: print(output.shape))
-    model.fc2.register_forward_hook(lambda m, input, output: print(output.shape))
-    model.fc3.register_forward_hook(lambda m, input, output: print(output.shape))
+    model.conv1.register_forward_hook(get_activation('conv1'))
+    model.activation_1.register_forward_hook(get_activation('activation_1'))
+    model.activation_2.register_forward_hook(get_activation('activation_2'))
+    model.activation_3.register_forward_hook(get_activation('activation_3'))
+    model.activation_4.register_forward_hook(get_activation('activation_4'))
+
     outputs = model(images)
+    print(activation.keys())
+    for key in activation.keys():
+        print(f'{key} shape: {activation[key].shape}')
 
 if __name__ == '__main__':
     main()
--- train/code/model.py
+++ train/code/model.py
@@ -9,15 +9,28 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
+        self.activation_1 = nn.ReLU()
+        self.activation_2 = nn.ReLU()
+        self.activation_3 = nn.ReLU()
+        self.activation_4 = nn.ReLU()
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x= self.conv1(x)
+        x = self.activation_1 (x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = self.activation_2(x)
+        x = self.pool(x)
+
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = self.fc1(x)
+        x = self.activation_3(x)
+
+        x = self.fc2(x)
+        x = self.activation_4(x)
         x = self.fc3(x)
         return x
