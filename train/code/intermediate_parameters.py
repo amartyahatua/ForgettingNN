1dcafb88650104b0414d4589208c35343f104949
Amartya <amartyahatua@gmail.com>
2024-01-06 00:14:26 -0500

    Updating

--- train/code/intermediate_parameters.py
+++ train/code/intermediate_parameters.py
@@ -3,7 +3,45 @@ import torch
 import numpy as np
 import torchvision
 import torchvision.transforms as transforms
-from model import Net
+# from train.code.model import Net
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.activation_1 = nn.ReLU()
+        self.activation_2 = nn.ReLU()
+        self.activation_3 = nn.ReLU()
+        self.activation_4 = nn.ReLU()
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x= self.conv1(x)
+        x = self.activation_1 (x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = self.activation_2(x)
+        x = self.pool(x)
+
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = self.fc1(x)
+        x = self.activation_3(x)
+
+        x = self.fc2(x)
+        x = self.activation_4(x)
+        x = self.fc3(x)
+        return x
+
 
 transform = transforms.Compose(
         [transforms.ToTensor(),
@@ -18,8 +56,19 @@ sorted_node = {}
 average = {}
 
 def get_index(df_avg):
-    df_avg = df_avg.values.tolist()
-    df_avg.sort()
+    df_avg_list = []
+    for i in df_avg[0]:
+        df_avg_list.append(i)
+
+    df_sort = df_avg_list
+    df_sort = sorted(df_sort, key=float, reverse=True)
+    out_index = []
+
+    for ele in df_sort:
+        index = df_avg_list.index(ele)
+        out_index.append(index)
+    ord_index = pd.DataFrame(out_index, columns=['Index'])
+    return ord_index
 
 
 def sort_nodes(activation, name):
@@ -34,7 +83,7 @@ def sort_nodes(activation, name):
     l = l.transpose()
     m = pd.DataFrame(average[name])
     ord_index = get_index(m)
-    n = pd.concat([l,m], axis=1)
+    n = pd.concat([l,m, ord_index], axis=1)
     n.to_csv(f'{name}_weights.csv')
 
 def get_activation(name):
