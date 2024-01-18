8e22058b215c4db8bff51d48496ead3244ab3bd6
Amartya <amartyahatua@gmail.com>
2024-01-17 23:21:31 -0500

    Ordering the nodes

--- train/code/train.py
+++ train/code/train.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
+import pandas as pd
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -28,20 +28,22 @@ classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
-
-
 class MyLayer(nn.Module):
     """
     This is a user defined neural network layer
     param: int input size
     param: tensor Input * random weight
     """
+
     def __init__(self, in_size):
         super().__init__()
         self.weight = None
         self.size = in_size
 
-    def forward(self, x, n_epochs, rank):
+    def forward(self, x, n_epochs, layer):
+        df = pd.read_csv('Order_out.csv')
+        rank = torch.tensor(df[layer].values[0:x.shape[1]])
+        rank = rank.float()
         self.weight = np.exp(-n_epochs / rank)
         return x * self.weight
 
@@ -59,25 +61,28 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
-        self.myLayer = MyLayer(120)
+        self.myLayer_3 = MyLayer(120)
+        self.myLayer_4 = MyLayer(84)
 
     def forward(self, x):
-        x= self.conv1(x)
-        x = self.activation_1 (x)
+        x = self.conv1(x)
+        x = self.activation_1(x)
         x = self.pool(x)
 
         x = self.conv2(x)
         x = self.activation_2(x)
         x = self.pool(x)
 
-        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = self.activation_3(x)
-        x = self.myLayer(x, self.epoch, self.rank)
+        x = self.myLayer_3(x, self.epoch, 'activation_3')
 
         x = self.fc2(x)
         x = self.activation_4(x)
+        x = self.myLayer_4(x, self.epoch, 'activation_4')
         x = self.fc3(x)
+
         return x
 
 
@@ -85,6 +90,7 @@ net = Net()
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
 
 def train():
     for epoch in range(2):  # loop over the dataset multiple times
@@ -98,8 +104,7 @@ def train():
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            net.rank = 1
-            net.epoch = epoch+1
+            net.epoch = epoch + 1
             outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
@@ -107,12 +112,13 @@ def train():
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
+            if i % 2000 == 1999:  # print every 2000 mini-batches
                 print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
                 running_loss = 0.0
     PATH = '../../model/cifar_net.pth'
     torch.save(net.state_dict(), PATH)
     print('Finished Training')
+
 
 if __name__ == '__main__':
     train()
