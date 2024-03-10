14be428698ebc09156e593145ab694ba48c534a8
Amartya <amartyahatua@gmail.com>
2024-03-10 00:02:21 -0500

    Adding code for four different types of rank calculation

--- mnist/classification_best.py
+++ mnist/classification_best.py
@@ -1,35 +1,15 @@
-import os
 import argparse
 import torch
+import numpy as np
 import torch.nn as nn
-import pandas as pd
 import torch.nn.functional as F
+import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 from matplotlib import pyplot as plt
-import random
-
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 torch.autograd.set_detect_anomaly(True)
-
-
-def get_index(df_avg):
-    L = []
-    for val in df_avg.values.tolist():
-        L.extend(val)
-    x = tuple(k[1] for k in sorted((x[1], j) for j, x in enumerate(
-        sorted((x, i) for i, x in enumerate(L)))))
-    ord_index = [max(x) - i for i in list(x)]
-    return ord_index
-
-
-def node_order(weights):
-    average = torch.mean(weights, axis=0)
-    new_average = pd.DataFrame(average.cpu().detach().numpy())
-    ord_index = get_index(new_average)
-    return ord_index
-
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -44,15 +24,15 @@ class Net(nn.Module):
         if self.type == 'unlearning':
             prev_drp = 0
             try:
-                self.dropout1 = nn.Dropout(0.25 - (0.072 * self.turn) + (0.05 * (self.epoch - 1)))
-                self.dropout2 = nn.Dropout(0.50 - (0.072 * self.turn) + (0.05 * (self.epoch - 1)))
-                prev_drp = (0.25 - (0.072 * self.turn) + (0.05 * (self.epoch - 1)))
+                self.dropout1 = nn.Dropout(0.25-(0.072*self.turn)+(0.05*(self.epoch-1)))
+                self.dropout2 = nn.Dropout(0.50-(0.072*self.turn)+(0.05*(self.epoch-1)))
+                prev_drp = (0.25-(0.072*self.turn)+(0.05*(self.epoch-1)))
             except:
                 self.dropout1 = nn.Dropout(prev_drp)
                 self.dropout2 = nn.Dropout(prev_drp)
         elif self.type == 'learning':
-            self.dropout1 = nn.Dropout(0.25 - (0.01 * self.turn))
-            self.dropout2 = nn.Dropout(0.50 - (0.01 * self.turn))
+            self.dropout1 = nn.Dropout(0.25-(0.01*self.turn))
+            self.dropout2 = nn.Dropout(0.50-(0.01*self.turn))
 
         x = self.conv1(x)
         x = F.relu(x)
@@ -64,61 +44,16 @@ class Net(nn.Module):
         x = self.fc1(x)
 
         if self.type == 'unlearning':
-            # With ordered numbers
             rank = torch.tensor([i for i in range(x.shape[1])])
-            rank = rank.to(self.device)
-
-            # # With ordered nodes
-            # rank = node_order(x)
-            # rank = torch.tensor(rank)
-            # rank = rank.to(self.device)
-            #
-            # # With top 30 nodes
-            # rank = node_order(rank)
-            # rank = [ind if 30 <= ind else 0 for ind in rank]
-            #
-            # # Random node
-            # random_numbers = random.sample(range(0, x.shape[1] - 1), random.randint(0, x.shape[1] - 1))
-            # rank = node_order(x)
-            # for rn in random_numbers:
-            #     try:
-            #         rank[rn] = 1
-            #     except:
-            #         continue
-            #     rank = torch.tensor(rank)
-            #     rank = rank.to(self.device)
-            x = x * torch.exp(-(self.forget_strength / rank))
+            x = x*torch.exp(-(self.epoch/rank))
 
         x = F.relu(x)
         x = self.dropout2(x)
         x = self.fc2(x)
 
         if self.type == 'unlearning':
-            # With ordered numbers
             rank = torch.tensor([i for i in range(x.shape[1])])
-            rank = rank.to(self.device)
-
-            # # With ordered nodes
-            # rank = node_order(x)
-            # rank = torch.tensor(rank)
-            # rank = rank.to(self.device)
-            #
-            # # With top 30 nodes
-            # rank = node_order(rank)
-            # rank = [ind if 30 <= ind else 0 for ind in rank]
-            #
-            # # Random node
-            # random_numbers = random.sample(range(0, x.shape[1] - 1), random.randint(0, x.shape[1] - 1))
-            # rank = node_order(x)
-            # for rn in random_numbers:
-            #     try:
-            #         rank[rn] = 1
-            #     except:
-            #         continue
-            #     rank = torch.tensor(rank)
-            #     rank = rank.to(self.device)
-            x = x * torch.exp(-(self.forget_strength / rank))
-
+            x = x*torch.exp(-(self.epoch/rank))
         output = F.log_softmax(x, dim=1)
         return output
 
@@ -126,11 +61,8 @@ class Net(nn.Module):
 def train(args, model, device, train_loader, optimizer, epoch, type, turn):
     model.train()
     model.type = type
-    model.forget_strength = epoch
+    model.epoch = epoch
     model.turn = turn
-    model.rank = 3
-    model.last_layer_forget = 0
-    model.device = device
 
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -142,15 +74,12 @@ def train(args, model, device, train_loader, optimizer, epoch, type, turn):
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.item()))
+                100. * batch_idx / len(train_loader), loss.item()))
             if args.dry_run:
                 break
 
-
 accuracy = []
 accuracy_num = []
-
-
 def test(model, device, test_loader):
     model.eval()
     test_loss = 0
@@ -181,7 +110,7 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=1, metavar='N',
+    parser.add_argument('--epochs', type=int, default=2, metavar='N',
                         help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=1.001, metavar='LR',
                         help='learning rate (default: 1.0)')
@@ -189,8 +118,8 @@ def main():
                         help='Learning rate step gamma (default: 0.7)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
-    # parser.add_argument('--no-mps', action='store_true', default=False,
-    #                     help='disables macOS GPU training')
+    parser.add_argument('--no-mps', action='store_true', default=False,
+                        help='disables macOS GPU training')
     parser.add_argument('--dry-run', action='store_true', default=False,
                         help='quickly check a single pass')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
@@ -201,13 +130,13 @@ def main():
                         help='For Saving the current Model')
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
-    # use_mps = not args.no_mps and torch.backends.mps.is_available()
+    use_mps = not args.no_mps and torch.backends.mps.is_available()
     torch.manual_seed(args.seed)
 
     if use_cuda:
         device = torch.device("cuda")
-    # elif use_mps:
-    #     device = torch.device("mps")
+    elif use_mps:
+        device = torch.device("mps")
     else:
         device = torch.device("cpu")
 
@@ -220,31 +149,33 @@ def main():
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
-    transform = transforms.Compose([
+    transform=transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
-    ])
+        ])
     dataset1 = datasets.MNIST('../data', train=True, download=True,
-                              transform=transform)
+                       transform=transform)
     dataset2 = datasets.MNIST('../data', train=False,
-                              transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
+                       transform=transform)
+    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = Net().to(device)
+    #optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+    # optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for turn in range(5):
+    for turn in range(10):
         print(f'------------------------Turn = {turn}-----------------------------')
         print(f'------------------------Learning-----------------------------')
-        for epoch in range(1, args.epochs + 2):
+        for epoch in range(1, args.epochs + 1):
             train(args, model, device, train_loader, optimizer, epoch, 'learning', turn)
             test(model, device, test_loader)
             scheduler.step()
         print(f'------------------------Unlearning-----------------------------')
         for epoch in range(1, args.epochs + 5):
-            train(args, model, device, train_loader, optimizer, epoch, 'unlearning', turn)
+            train(args, model, device, train_loader, optimizer, epoch, 'unlearning',turn)
             test(model, device, test_loader)
             scheduler.step()
 
@@ -255,10 +186,8 @@ def main():
 if __name__ == '__main__':
     main()
     print(accuracy)
-    accuracy = pd.DataFrame(accuracy)
-    accuracy.to_csv('Result_Rank_in_order_all_epoch_all_layer.csv', index=False)
     plt.plot(accuracy)
-    plt.savefig('plots/unlearning_plot_rank_in_order_all_epoch_all_layer.png')
+    plt.savefig('plots/unlearning_plot_best.png')
 
     plt.plot(accuracy_num)
-    plt.savefig('plots/unlearning_rank_in_order_all_epoch_all_layer.png')
+    plt.savefig('plots/unlearning_number_plot.png')
